91c010aa8b0906985ccfbf350f6b77c5aa1bdb63
Bhanubhakta-Poudel <sandeepaudel1122@gmail.com>
2025-08-29 21:48:31 +0545

    about page

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-
-from .views import home_view
+# from.views import about
+# from.views import home_view
+from.import views
 
 urlpatterns = [
-    path('', home_view)
+    path('', views.home_view, name='home_view'),
+    path('about/',views.about,name='about')
 ]
