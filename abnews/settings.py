a7b7ff5faf5912e760315ecdf3510c5ee160c0b3
amrit <peaceamrit95@gmail.com>
2025-08-31 11:21:11 +0545

    mysql database

--- abnews/settings.py
+++ abnews/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'abnews.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.mysql',
+                'NAME': 'abnews',
+                'USER': 'root',
+                'PASSWORD': 'root',
+                'HOST': 'localhost',  # Or the IP/hostname of your MySQL server
+                'PORT': '3306',
+            }
+        }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
