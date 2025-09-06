9e7b2df1de79a11fea5b49ed711dda004241828d
amrit <peaceamrit95@gmail.com>
2025-09-06 20:12:39 +0545

    reverse relation urlencode

--- home/admin.py
+++ home/admin.py
@@ -1,23 +1,31 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
 from django.db.models import Count
 
 # Register your models here.
 from . import models
 
 @admin.register(models.News)
-
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'likes', 'date', 'image', 'comment_count')  
 
     
-    def comment_count(self, obj):
-        return obj.comment_count
     
+    def comment_count(self, obj):
+        link = (reverse('admin:home_comment_changelist')
+                + '?'
+                + urlencode({
+                    'post_id': str(obj.id)
+                }))
+        return format_html('<a href="{}">{} comments</a>', link, obj.comment_count)
+        
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
             comment_count=Count('comment')
             
             )
+
 
     
 
@@ -26,5 +34,5 @@ class NewsAdmin(admin.ModelAdmin):
 
 @admin.register(models.Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name','body', 'date')   
+    list_display = ('id', 'name','body', 'date', 'post')   
 
